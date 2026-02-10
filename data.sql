8c72d0df1e9f68e553c6e1d2d143013b437a41f6
Fahd Taher Al-Shehari <fahdtaherali@gmail.com>
2026-02-10 10:32:52 +0300

    Feat: Add student score filtering logic

---
+++ data.sql
@@ -0,0 +1,12 @@
+use alriyadiat_amaluna;
+select 6 > 5;
+select 1 and 0;
+select 2 * 2;
+select * from student where student_score > 99;
+
+update student 
+set student_score = 100
+where id = 1;
+delete from student where id = 3;
+select * from student order by student_name ASC;
+
